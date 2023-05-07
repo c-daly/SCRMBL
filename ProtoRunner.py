e3c31f3bb6ba165a627b6c4ac0e8a4d1238b077d
c-daly <chris@cdaly.me>
2023-05-07 10:22:50 -0400

    Started refactoring into a more sane project structure

--- ProtoRunner.py
+++ ProtoRunner.py
@@ -2,7 +2,7 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
 from envs.SC2SyncEnv import SC2SyncEnv
 from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3 import A2C
+from stable_baselines3 import A2C, PPO
 from absl import flags
 from contextlib import closing
 from time import sleep
@@ -19,17 +19,17 @@ with closing(create_connection("ws://127.0.0.1:5000/sc2api")) as websocket:
     env = SC2SyncEnv(websocket)
     env = DummyVecEnv([lambda: Monitor(env)])
     eval_callback = EvalCallback(env, best_model_save_path="./logs/raw_agent_runner/a2c/",
-                                 log_path="./logs/raw_agent_runner/a2c/", eval_freq=10,
+                                 log_path="./logs/raw_agent_runner/a2c/", eval_freq=100,
                                  deterministic=True, render=False,)
 
-    model = A2C('MlpPolicy', env, learning_rate=0.001, gamma=0.85, verbose=1)
+    model = A2C('MlpPolicy', env, learning_rate=0.0000000001, gamma=0.99999, verbose=1)
     #model = A2C.load("a2c_sc2_dbz")
     #model = A2C.load("logs/raw_agent_runner/a2c/best_model")
     model.env = env
     #env.reset()
     while True:
         try:
-            model.learn(total_timesteps=100, callback=[checkpoint_callback, eval_callback])
+            model.learn(total_timesteps=10000000, callback=[eval_callback, checkpoint_callback])
         except Exception as e:
             continue
             env.reset()
---
+++ managers/SC2ProcessManager.py
@@ -0,0 +1,70 @@
+from s2clientprotocol import sc2api_pb2 as sc_pb
+from s2clientprotocol import raw_pb2 as raw_pb
+from s2clientprotocol import common_pb2 as common_pb
+class SC2ProcessManager(object):
+    def __init__(self, websocket):
+        self.websocket = websocket
+
+    def create_game(self):
+        response = None
+        create_game = sc_pb.RequestCreateGame(
+            realtime=False,
+            # local_map=sc_pb.LocalMap(map_path="melee/Simple64.SC2Map")
+            local_map=sc_pb.LocalMap(map_path="mini_games/DefeatZerglingsAndBanelings.SC2Map"),
+            # player_setup=[
+            #    sc_pb.PlayerSetup(type=sc_pb.Participant)  # Human
+            #    #sc_pb.PlayerSetup(type=sc_pb.Computer)  # Human
+            # ]
+        )
+
+        create_game.player_setup.add(type=sc_pb.Participant, race=common_pb.Terran)
+        create_game.player_setup.add(type=sc_pb.Computer, race=common_pb.Zerg, difficulty=sc_pb.Easy)
+        request = sc_pb.Request(create_game=create_game)
+        try:
+            self.websocket.send(request.SerializeToString())
+            response_data = self.websocket.recv()
+            response = sc_pb.Response.FromString(response_data)
+        except Exception as e:
+            print(f"Create game failure: {e}")
+
+        if len(response.error) > 0:
+            print(f"Error: {response.error}")
+            return
+
+        # Join the game
+        join_game = sc_pb.RequestJoinGame(
+            race=common_pb.Terran,
+            options=sc_pb.InterfaceOptions(
+                raw=True,
+
+            )
+        )
+        request = sc_pb.Request(join_game=join_game)
+        try:
+            self.websocket.send(request.SerializeToString())
+            response_data = self.websocket.recv()
+            response = sc_pb.Response.FromString(response_data)
+
+            if len(response.error) > 0:
+                print(f"Error: {response.error}")
+                return
+        except Exception as e:
+            print(f"Join Error {e}")
+
+    def get_obs(self):
+        request = sc_pb.Request(observation=sc_pb.RequestObservation())
+        self.websocket.send(request.SerializeToString())
+        response_data = self.websocket.recv()
+        response = sc_pb.Response.FromString(response_data)
+        return response.observation
+
+
+    def step(self):
+        # Step the game forward by a single step
+        request_step = sc_pb.RequestStep(count=2)
+        request = sc_pb.Request(step=request_step)
+        self.websocket.send(request.SerializeToString())
+        response_data = self.websocket.recv()
+
+        response = sc_pb.Response.FromString(response_data)
+        return response
